2852ee4d4a0397bb80ed21da48e1b8dc1b8a71e7
““Hazards” <799896781@qq.com>
2019-08-09 16:06:01 +0800

    9-2通过django内置的login完成登录

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -15,11 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 import xadmin
 
+from apps.users.views import LoginView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('ueditor/', include('DjangoUeditor.urls')),
-    path('xadmin/', xadmin.site.urls)
+    #path('admin/', admin.site.urls),
+    #path('ueditor/', include('DjangoUeditor.urls')),
+    path('xadmin/', xadmin.site.urls),
+    path('', TemplateView.as_view(template_name="index.html"), name="index"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('register/', TemplateView.as_view(template_name="register.html"), name="register"),
+
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.contrib.auth import authenticate, login  # 自带的权限认证函数
+from django.http import HttpResponseRedirect    # url重定向
+from django.urls import reverse
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'login.html')
+
+    def post(self, request, *args, **kwargs):
+        user_name = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        user = authenticate(username=user_name, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))  # 跳转url地址
+        else:
+            return render(request, 'login.html', {
+                'msg': "用户名或密码错误"
+            })
+
+
